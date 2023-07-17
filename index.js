231e43138302069e49ed47bd4325c9c8cbc5f5e3
MetalKJ <baggio630@gmail.com>
2023-07-18 00:32:46 +0800

    .

--- index.js
+++ index.js
@@ -17,53 +17,38 @@ console.log(response);
 const app = new Koa()
 const router = new Router();
 
+// router.get("/chat", async (ctx, next) => {
+//     // 获取请求中的参数
+//     const { prompt } = ctx.request.query;
 
-
+//     const res = await openai.createCompletion({
+//         // 对话模型
+//         model: "text-davinci-003",
+//         //model: "gpt-3.5-turbo-16k-0613",
+//         prompt: prompt,
+//         max_tokens: 2048,
+//         temperature: 0.2
+//     })
+//     // 将生成的内容返回给客户端
+//     ctx.body = res.data.choices
+// });
 
 router.get("/chat", async (ctx, next) => {
     // 获取请求中的参数
     const { prompt } = ctx.request.query;
 
-    const res = await openai.createCompletion({
+    const res = await openai.createChatCompletion({
         // 对话模型
-        model: "text-davinci-003",
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
         //model: "gpt-3.5-turbo-16k-0613",
-        prompt: prompt,
-        max_tokens: 2048,
-        temperature: 0.2
+        // prompt: prompt,
+        // max_tokens: 2048,
+        // temperature: 0.2
     })
     // 将生成的内容返回给客户端
     ctx.body = res.data.choices
-});
-
-// router.get("/chat", async (ctx, next) => {
-//     try {
-//         // 获取请求中的参数
-//         const { prompt } = ctx.request.query;
-
-//     const res = await openai.createCompletion({
-//         // 对话模型
-//         // model: "text-davinci-003",
-//         model: "gpt-3.5-turbo",
-//         prompt: prompt,
-//         max_tokens: 2048,
-//         temperature: 0.2
-//     })
-//         // 将生成的内容返回给客户端
-//         if (!res || !res.data || !res.data.choices) {
-//             console.error('Unexpected response from OpenAI API', res);
-//             ctx.status = 500;
-//             ctx.body = 'Unexpected response from OpenAI API';
-//             return;
-//         }
-//         ctx.body = res.data.choices;
-//     } catch (error) {
-//         console.error(`Error when trying to use OpenAI API: ${error.message}`);
-//         // console.error(`Error when trying to use OpenAI API2: ${error.response.data.error.message}`);
-//         ctx.status = 500;
-//         ctx.body = 'Internal Server Error';
-//     }
-// });
+}); 
 
 app.use(cors({
   origin: '*'
