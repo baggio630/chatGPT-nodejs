395fdb7ab2d2f506bdd2ac743d74977442646a86
蒂尼 <jinzhuo.jz@cainiao.com>
2023-11-07 15:51:23 +0800

    .

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-// import { Configuration, OpenAIApi } from "openai";
-import OpenAI from "openai";
+import { Configuration, OpenAIApi } from "openai";
 import Koa from "koa";
 import Router from "koa-router";
 import cors from "@koa/cors";
-// import bodyParser from "koa-bodyparser";
 
-// const configuration = new Configuration({
-//   organization: process.env.APP_ORG,
-//   apiKey: process.env.APP_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
+// https://platform.openai.com/docs/api-reference/images
 
-const openai = new OpenAI({organization: process.env.APP_ORG, apiKey: process.env.APP_KEY, });
+const configuration = new Configuration({
+  organization: process.env.APP_ORG,
+  apiKey: process.env.APP_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines();
 
-// const response = await openai.listEngines();
-
-// console.log("prompt", response);
+console.log("prompt", response);
 
 const app = new Koa();
 const router = new Router();
@@ -32,8 +29,6 @@ router.get("/chat", async (ctx, next) => {
     messages.unshift({role: "system", content: systemMessage});
   }
 
-  console.log("提交的数据",messages);
-
   const res = await openai.createChatCompletion({
     model: "gpt-3.5-turbo-16k-0613",
     messages: messages,
@@ -41,28 +36,6 @@ router.get("/chat", async (ctx, next) => {
   });
   ctx.body = res.data.choices;
 });
-
-router.post("/assistant", async (ctx) => {
-  // const { prompt } = ctx.request.body;
-  const prompt = "你能做什么？";
-
-  // 创建一个新的线程
-  const thread = await openai.beta.threads.create();
-
-  // 在线程中创建并执行一个运行
-  const run = await openai.beta.threads.runs.create(
-    {
-    thread_id: thread.id,
-    assistant_id: "asst_oaoORJy5U49dZrdwQvkYVbIb",
-    inputs: prompt
-    }
-  );
-
-  // 将回复消息作为响应返回
-  ctx.body = run.data;
-});
-
-// console.log("结果",res);
 
 app.use(
   cors({
@@ -73,10 +46,10 @@ app.use(
 // 启用路由
 app.use(router.routes()).use(router.allowedMethods());
 
-// router.get("/engines", async (ctx, next) => {
-//   const response = await openai.listEngines();
-//   ctx.body = response.data; // 返回模型列表到客户端
-// });
+router.get("/engines", async (ctx, next) => {
+  const response = await openai.listEngines();
+  ctx.body = response.data; // 返回模型列表到客户端
+});
 
 // 启动服务器
 app.listen(process.env.PORT, () => {
